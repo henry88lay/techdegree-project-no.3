0c26fc4f4e4d2851298319526651b0d41d48a787
Henry Lay <henry88lay@gmail.com>
2019-02-06 09:29:29 +1100

    Major Changes towards the project

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Global Variables
+
+// Shirt color
+const $tshirtColor = $("#colors-js-puns");
+const $jsPuns = $("#color option:contains('Puns')");
+const $heart = $("#color option:contains('(I')");
+
+
+// * Objective 1 Set focus on the first text field * //
+$('#name').focus();
+
+// * Objective 2 ”Job Role” section of the form * //
+
+
+// * Objective 3 ”T-Shirt Info” section of the form * //
+
+
+// * Objective 4 ”Register for Activities” section of the form * //
+
+
+// * Objective 5 Payment Info section of the form * //
+
+
+// * Objective 6 Form validation and messages * //
