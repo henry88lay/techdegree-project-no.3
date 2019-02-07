d72c8e573b8adcc2f2207602fe2882ce8f41ae4f
Henry Lay <henry88lay@gmail.com>
2019-02-07 17:12:42 +1100

    Update
    completed up to objective 4

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-// Global Variables
+// * Objective 1 Set focus on the first text field * //
+$('#name').focus();
 
+// * Objective 2 ”Job Role” section of the form * //    
+    // = other-title entered into HTML and hiddle unless Other is selected
+    $('#other-title').hide();
+
+    $('div #title').on('change', function() {
+        const value = $(this).val();
+        if(value === 'other') {
+            $('#other-title').show();
+        } else {
+            $('#other-title').hide();
+        }
+    });
+
+// * Objective 3 ”T-Shirt Info” section of the form * //
 // Shirt color
 const $tshirtColor = $("#colors-js-puns");
 const $jsPuns = $("#color option:contains('Puns')");
 const $heart = $("#color option:contains('(I')");
 
+$(tshirtColor).hide();
 
-// * Objective 1 Set focus on the first text field * //
-$('#name').focus();
-
-// * Objective 2 ”Job Role” section of the form * //
-
-
-// * Objective 3 ”T-Shirt Info” section of the form * //
-
+const dropDownBox = ($optionOne, $optionTwo) => {
+    $tshirtColor.show();
+    $optionOne.hide();
+    $optionTwo.show();
+    $optionTwo.first().attr('selected', 'selected');
+    $optionOne.first().attr('selected',false);
+};
 
 // * Objective 4 ”Register for Activities” section of the form * //
 
